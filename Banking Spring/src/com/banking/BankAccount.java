df77f7bb0d2a2915e8741440bf30c8bb617fabe2
sburugu <sburugu@BLRTRNGGF01008.corp.capgemini.com>
2019-03-05 13:44:30 +0530

    Spring 3

---
+++ Banking Spring/src/com/banking/BankAccount.java
@@ -0,0 +1,46 @@
+package com.banking;
+
+public class BankAccount {
+	
+	private String accountId;
+	private String accountHolderName;
+	private String accountType;
+	private double accountBalance;
+	public BankAccount(String accountId, String accountHolderName, String accountType, double accountBalance) {
+		super();
+		this.accountId = accountId;
+		this.accountHolderName = accountHolderName;
+		this.accountType = accountType;
+		this.accountBalance = accountBalance;
+	}
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	@Override
+	public String toString() {
+		return "BankAccount [accountId=" + accountId + ", accountHolderName=" + accountHolderName + ", accountType="
+				+ accountType + ", accountBalance=" + accountBalance + "]";
+	}
+
+}
